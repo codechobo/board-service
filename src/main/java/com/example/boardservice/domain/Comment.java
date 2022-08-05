1ba497ecd9800bbc9d31b9476433e8e98f3bd5f1
이경범 <tkdwk567@naver.com>
2022-08-06 01:28:41 +0900

    Comment 도메인 개발

--- src/main/java/com/example/boardservice/domain/Comment.java
+++ src/main/java/com/example/boardservice/domain/Comment.java
@@ -13,18 +13,31 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "COMMENTS")
 @Entity
 public class Comment extends TimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENTS_ID")
     private Long id;
 
     @Column(name = "AUTHOR", length = 30)
     private String author; // 작성자
 
+    @Column(name = "CONTENT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
     private Comment parent;
+
+    @OneToMany(mappedBy = "parent")
     private List<Comment> child = new ArrayList<>(); // 댓글
+
+    public Comment(String author) {
+        this.author = author;
+    }
 
     public void addComment(Comment comment) {
         this.parent = comment;
