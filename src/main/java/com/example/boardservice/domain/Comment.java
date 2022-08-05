399eae0ea02da6e600b03e61383e5fd6d67e0efb
이경범 <tkdwk567@naver.com>
2022-08-06 01:42:48 +0900

    연관관계 매핑

--- src/main/java/com/example/boardservice/domain/Comment.java
+++ src/main/java/com/example/boardservice/domain/Comment.java
@@ -3,10 +3,13 @@ package com.example.boardservice.domain;
 
 import com.example.boardservice.domain.base.TimeEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,17 +32,28 @@ public class Comment extends TimeEntity {
     private String content;
 
     @ManyToOne
+    @JoinColumn(name = "POSTS_ID")
+    private Post post;
+
+    @ManyToOne
     @JoinColumn(name = "PARENT_ID")
     private Comment parent;
 
     @OneToMany(mappedBy = "parent")
-    private List<Comment> child = new ArrayList<>(); // 댓글
+    private List<Comment> child = new ArrayList<>();
 
-    public Comment(String author) {
+    @Builder
+    public Comment(String author, String content) {
         this.author = author;
+        this.content = content;
     }
 
-    public void addComment(Comment comment) {
+    public void addComment(Post post, Comment comment) {
+        if (post == null || comment == null) {
+            throw new EntityNotFoundException("엔티티를 찾을 수 없습니다.");
+        }
+
+        this.post = post;
         this.parent = comment;
         this.child.add(comment);
     }
