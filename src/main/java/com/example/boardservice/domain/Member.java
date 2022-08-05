bc57eb873a075f993b97e3f237ee821e2062e285
이경범 <tkdwk567@naver.com>
2022-08-06 00:29:40 +0900

    member 도메인 개발

---
+++ src/main/java/com/example/boardservice/domain/Member.java
@@ -0,0 +1,37 @@
+package com.example.boardservice.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "MEMBERS", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "NICKNAME"),
+        @UniqueConstraint(columnNames = "PASSWORD"),
+        @UniqueConstraint(columnNames = "EMAIL")})
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBERS_ID")
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name; // 이름
+
+    @Column(name = "NICKNAME", length = 30)
+    private String nickName; // 닉네임
+
+    @Column(name = "EMAIL", length = 100, nullable = false)
+    private String email; // 이메일
+
+    @Column(name = "PASSWORD", length = 100, nullable = false)
+    private String password; // 비밀번호
+
+}
