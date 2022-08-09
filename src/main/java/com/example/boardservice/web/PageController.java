7f3729d7e3b1cfe99e1ba39a840b52e5b5c5816b
이경범 <tkdwk567@naver.com>
2022-08-09 15:44:56 +0900

    회원가입 어드민 생성

---
+++ src/main/java/com/example/boardservice/web/PageController.java
@@ -0,0 +1,18 @@
+package com.example.boardservice.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+
+    @GetMapping("/members/save")
+    public String memberSave() {
+        return "member-save";
+    }
+}
