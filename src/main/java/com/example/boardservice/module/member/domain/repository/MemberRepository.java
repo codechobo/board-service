c42e5f9c840064e4f4a353a6b57a5250acc1cd54
이경범 <tkdwk567@naver.com>
2022-08-26 00:02:35 +0900

    회원 업데이트 기능 테스트 리팩터링 - 서비스 테스트 마무리 - jpa 의 기본적인 crud 는 믿고 넘어간다. - 하지만 비지니스 로직은 꼭 테스트를 진행해야 한다.

--- src/main/java/com/example/boardservice/module/member/domain/repository/MemberRepository.java
+++ src/main/java/com/example/boardservice/module/member/domain/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package com.example.boardservice.module.member.domain.repository;
 
 import com.example.boardservice.module.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional(readOnly = true)
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
 
     boolean existsByNicknameAndEmailAndPassword(String nickname, String email, String password);
