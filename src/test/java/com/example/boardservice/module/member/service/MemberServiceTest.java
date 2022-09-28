558ee452c0b5159ad3e81b79e1dcb8c0faccaf32
이경범 <tkdwk567@naver.com>
2022-09-29 00:03:23 +0900

    Member nickname update test

--- src/test/java/com/example/boardservice/module/member/service/MemberServiceTest.java
+++ src/test/java/com/example/boardservice/module/member/service/MemberServiceTest.java
@@ -5,6 +5,7 @@ import com.example.boardservice.factory.RequestDtoFactory;
 import com.example.boardservice.module.member.domain.Member;
 import com.example.boardservice.module.member.domain.repository.MemberRepository;
 import com.example.boardservice.module.member.exception.NotMatchPasswordException;
+import com.example.boardservice.module.member.web.dto.RequestNicknameUpdateDto;
 import com.example.boardservice.module.member.web.dto.request.RequestMemberSaveDto;
 import com.example.boardservice.module.member.web.dto.request.RequestPasswordUpdateDto;
 import com.example.boardservice.module.member.web.dto.response.ResponseMembersPageDto;
@@ -27,10 +28,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MemberServiceTest {
 
-    @Autowired MemberRepository memberRepository;
-    @Autowired MemberService memberService;
-    @Autowired PasswordEncoder passwordEncoder;
-
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Test
     @DisplayName("회원 정보 저장 테스트 성공")
@@ -40,12 +43,8 @@ class MemberServiceTest {
         String email = "기영@naver.com";
         String password = "test1234";
 
-        RequestMemberSaveDto dto = RequestDtoFactory.createMemberSaveRequestDto();
-        Member member = createMember(dto);
-        memberRepository.save(member);
-
         // when
-        Member findMember = memberRepository.findByNickname(nickname).get();
+        Member member = saveMember();
 
         // then
         assertNotNull(member);
@@ -59,8 +58,7 @@ class MemberServiceTest {
     void saveMember_fail() {
         // given
         RequestMemberSaveDto dto = RequestDtoFactory.createMemberSaveRequestDto();
-        Member member = createMember(dto);
-        memberRepository.save(member);
+        saveMember();
 
         // when && then
         assertThatThrownBy(() -> memberService.saveMember(dto))
@@ -72,9 +70,7 @@ class MemberServiceTest {
     @DisplayName("회원 이름으로 검색하여 가입 시간 기준으로 최신으로 정렬 한 다음 회원 페이지 리스트 가져오기 테스트")
     void getMemberListIncludingLastJoin() {
         // given
-        RequestMemberSaveDto dto = RequestDtoFactory.createMemberSaveRequestDto();
-        Member member = createMember(dto);
-        memberRepository.save(member);
+        saveMember();
 
         // when
         ResponseMembersPageDto responseMembersPageDto = memberService.getMemberListIncludingLastJoin("이기영", Pageable.ofSize(5));
@@ -91,19 +87,17 @@ class MemberServiceTest {
     @DisplayName("회원 비밀번호 업데이트 테스트 성공")
     void updateAfterFindMember_success() {
         // given
-        RequestMemberSaveDto dto = RequestDtoFactory.createMemberSaveRequestDto();
-        Member member = createMember(dto);
-        Member saveMember = memberRepository.save(member);
+        Member member = saveMember();
 
         RequestPasswordUpdateDto passwordUpdateDto = new RequestPasswordUpdateDto();
         passwordUpdateDto.setNewPassword("test12345");
         passwordUpdateDto.setNewPasswordConfirm("test12345");
 
         // when
-        memberService.updatePasswordAfterFindMember(saveMember.getId(), passwordUpdateDto);
+        memberService.updatePasswordAfterFindMember(member.getId(), passwordUpdateDto);
 
         // then
-        Member findMember = memberRepository.findById(saveMember.getId()).get();
+        Member findMember = memberRepository.findById(member.getId()).get();
         assertTrue(passwordEncoder.matches(passwordUpdateDto.getNewPassword(), findMember.getPassword()));
     }
 
@@ -111,29 +105,57 @@ class MemberServiceTest {
     @DisplayName("회원 비밀번호가 일치하지 않는 경우 업데이트 실패")
     void updateAfterFindMember_fail() {
         // given
-        RequestMemberSaveDto dto = RequestDtoFactory.createMemberSaveRequestDto();
-        Member member = createMember(dto);
-        Member saveMember = memberRepository.save(member);
+        Member member = saveMember();
 
         RequestPasswordUpdateDto passwordUpdateDto = new RequestPasswordUpdateDto();
         passwordUpdateDto.setNewPassword("test123");
         passwordUpdateDto.setNewPasswordConfirm("test12345");
 
         // when && then
-        assertThatThrownBy(() -> memberService.updatePasswordAfterFindMember(saveMember.getId(), passwordUpdateDto))
+        assertThatThrownBy(() -> memberService.updatePasswordAfterFindMember(member.getId(), passwordUpdateDto))
                 .isInstanceOf(NotMatchPasswordException.class)
                 .overridingErrorMessage(ErrorCode.NOT_MATCH_PASSWORD.getMessage());
+    }
+
+    @Test
+    @DisplayName("회원 정보 닉네임 업데이트 성공")
+    void updateNicknameAfterFindMember_success() {
+        // given
+        Member member = saveMember();
+        RequestNicknameUpdateDto updateDto = RequestNicknameUpdateDto.builder()
+                .nickname("라면부자")
+                .build();
+
+        // when
+        memberService.updateNicknameAfterFindMember(member.getId(), updateDto);
+
+        // then
+        Member updateMember = memberRepository.findById(member.getId()).get();
+        assertEquals(member.getNickname(), updateMember.getNickname());
+    }
+
+    @Test
+    @DisplayName("회원 닉네임 정보가 중복된 닉네임으로 업데이트 한 경우 실패 -> 에러 던짐")
+    void updateNicknameAfterFindMember_fail() {
+        // given
+        Member member = saveMember();
+        RequestNicknameUpdateDto updateDto = RequestNicknameUpdateDto.builder()
+                .nickname(member.getNickname())
+                .build();
+
+        // when && then
+        assertThatThrownBy(() -> memberService.updateNicknameAfterFindMember(member.getId(), updateDto))
+                .isInstanceOf(DuplicateRequestException.class)
+                .hasMessage(ErrorCode.REQUEST_DATA_DUPLICATED.getMessage());
     }
 
     @Test
     @DisplayName("회원 정보 삭제 테스트 성공")
     void removeMember_success() {
         // given
-        RequestMemberSaveDto dto = RequestDtoFactory.createMemberSaveRequestDto();
-        Member member = createMember(dto);
-        Member saveMember = memberRepository.save(member);
+        Member member = saveMember();
 
-        memberService.removeMember(saveMember.getId());
+        memberService.removeMember(member.getId());
 
         // when && then
         assertThatThrownBy(() -> memberRepository.findById(1L)
@@ -149,5 +171,11 @@ class MemberServiceTest {
                 .email(dto.getEmail())
                 .password(passwordEncoder.encode(dto.getPassword()))
                 .build();
+    }
+
+    private Member saveMember() {
+        RequestMemberSaveDto dto = RequestDtoFactory.createMemberSaveRequestDto();
+        Member member = createMember(dto);
+        return memberRepository.save(member);
     }
 }
