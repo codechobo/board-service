cb6b46d848998b50500143a5ed0b07637215b788
이경범 <tkdwk567@naver.com>
2022-10-06 20:26:58 +0900

    member test

--- src/test/java/com/example/boardservice/module/member/web/MemberControllerTest.java
+++ src/test/java/com/example/boardservice/module/member/web/MemberControllerTest.java
@@ -18,14 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import javax.persistence.EntityNotFoundException;
-import java.util.Objects;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -60,63 +52,37 @@ class MemberControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
+
+    @WithMockUser
     @Test
     @DisplayName("회원 정보를 저장하는데 중복된 정보가 저장되면 예외가 발생하며 저장이 되지 않는다.")
     void createMember_Exception_Fail() throws Exception {
-        RequestMemberSaveDto memberSaveRequestDto = RequestMemberSaveDto.builder()
-                .name("이기영")
-                .nickname("까까머리")
-                .email("기영@naver.com")
-                .password("test1234")
-                .build();
-        given(memberService.saveMember(any(RequestMemberSaveDto.class)))
-                .willThrow(new EntityNotFoundException(ErrorCode.NOT_FOUND_ENTITY.getMessage()));
+        RequestMemberSaveDto requestDto = RequestDtoFactory.createMemberSaveRequestDto();
+        memberRepository.save(requestDto.toEntity());
 
-
-        mockMvc.perform(post("/api/v1/members")
+        mockMvc.perform(post("/members")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(memberSaveRequestDto)))
+                        .content(objectMapper.writeValueAsString(requestDto))
+                        .with(csrf()))
                 .andDo(print())
                 .andExpect(status().is(ErrorCode.REQUEST_DATA_DUPLICATED.getStatus().value()));
     }
 
+    @WithMockUser
     @Test
     @DisplayName("엔티티를 조회하는데 성공한다.")
     void readMemberById_Success() throws Exception {
         // given
-        RequestMemberSaveDto memberSaveRequestDto = RequestMemberSaveDto.builder()
-                .name("이기영")
-                .nickname("까까머리")
-                .email("기영@naver.com")
-                .password("test1234")
-                .build();
-        Member member = memberSaveRequestDto.toEntity();
+        RequestMemberSaveDto requestDto = RequestDtoFactory.createMemberSaveRequestDto();
+        Member member = requestDto.toEntity();
+
+        Member savedMember = memberRepository.save(member);
         ResponseMemberSaveDto memberSaveResponseDto = ResponseMemberSaveDto.builder().member(member).build();
-        given(memberService.findMemberById(anyLong())).willReturn(memberSaveResponseDto);
 
         // when && then
-        mockMvc.perform(get("/api/v1/members/1"))
+        mockMvc.perform(get("/members/" + savedMember.getId()))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(memberSaveResponseDto)));
-    }
-
-    @Test
-    @DisplayName("엔티티를 찾지 못해 예외가 발생하며 조회에 실패한다.")
-    void readMemberById_Fail() throws Exception {
-        // given
-        given(memberService.findMemberById(anyLong()))
-                .willThrow(new EntityNotFoundException(ErrorCode.NOT_FOUND_ENTITY.getMessage()));
-
-        // when && then
-        mockMvc.perform(get("/api/v1/members/1"))
-                .andDo(print())
-                .andExpect(status().is(ErrorCode.NOT_FOUND_ENTITY.getStatus().value()))
-                .andExpect(result ->
-                        assertTrue(Objects.requireNonNull(result.getResolvedException())
-                                .getClass().isAssignableFrom(EntityNotFoundException.class)))
-                .andExpect(result ->
-                        assertEquals(Objects.requireNonNull(result.getResolvedException()).getMessage(),
-                                ErrorCode.NOT_FOUND_ENTITY.getMessage()));
     }
 }
