6a039405e9cc501c540dbee070c08e58b735ea73
이경범 <tkdwk567@naver.com>
2022-10-25 14:09:17 +0900

    중복되는 HashTag 들은 어떻게 할 것인가 ??? 고려 해야 한당...

--- src/main/java/com/example/boardservice/module/post/service/PostService.java
+++ src/main/java/com/example/boardservice/module/post/service/PostService.java
@@ -48,7 +48,7 @@ public class PostService {
         return ResponsePostSaveDto.of(savePost);
     }
 
-    // 메서드 로직 고려할 것
+    // TODO 중복되는 HashTag 는 어떻게 처리할지 고려할 것
     private void addHashTag(RequestPostSaveDto requestDto, Post post) {
         List<String> hashTagNames = requestDto.getHashTagNames();
 
