9a81605606c15b40c44f4f8d3cd5b5f5383a25cf
이경범 <tkdwk567@naver.com>
2022-10-25 14:36:42 +0900

    게시글 저장 시 응답 Dto 에 HashTagDto 추가하여 리팩터링

---
+++ src/main/java/com/example/boardservice/module/post/web/dto/ResponsePostWithHashTagSaveDto.java
@@ -0,0 +1,26 @@
+package com.example.boardservice.module.post.web.dto;
+
+import com.example.boardservice.module.hashtag.web.dto.ResponseHashTagSaveDto;
+import com.example.boardservice.module.post.web.dto.response.ResponsePostSaveDto;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ResponsePostWithHashTagSaveDto {
+
+    private final ResponseHashTagSaveDto responseHashTagSaveDto;
+    private final ResponsePostSaveDto responsePostSaveDto;
+
+    @Builder
+    public ResponsePostWithHashTagSaveDto(ResponseHashTagSaveDto responseHashTagSaveDto, ResponsePostSaveDto responsePostSaveDto) {
+        this.responseHashTagSaveDto = responseHashTagSaveDto;
+        this.responsePostSaveDto = responsePostSaveDto;
+    }
+
+    public static ResponsePostWithHashTagSaveDto of(ResponseHashTagSaveDto responseHashTagSaveDto, ResponsePostSaveDto responsePostSaveDto) {
+        return ResponsePostWithHashTagSaveDto.builder()
+                .responseHashTagSaveDto(responseHashTagSaveDto)
+                .responsePostSaveDto(responsePostSaveDto)
+                .build();
+    }
+}
